5b7cd8edca58b730ba1b0ea92d2f9efb02dc5747
AntagonistArmy <117744215+AntagonistArmy@users.noreply.github.com>
2026-09-04 16:41:46 -0500

    Fold Omega Recursion into the living field

--- src/lib/field/index.ts
+++ src/lib/field/index.ts
@@ -1,3 +1,4 @@
 export { field, FieldFabric } from "./fabric.ts";
 export type { FieldSnapshot, FieldEvent, EventType } from "./fabric.ts";
 export { ORIGIN_ID, fnvHex, shortId } from "./event.ts";
+export { OMEGA_RECURSION_SOURCE, OMEGA_RECURSION_TRANSFORM } from "./omega-recursion.ts";
